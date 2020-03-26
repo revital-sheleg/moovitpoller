d46e7300287f4009c882874ab3ade3f461155e23
Administrator <azorevi@gmail.com>
2020-03-26 10:24:29 +0200

    add files

---
+++ poller/src/test/java/moovit/poller/PollerApplicationTests.java
@@ -0,0 +1,13 @@
+package moovit.poller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
